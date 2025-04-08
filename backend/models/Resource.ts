f74964ce3fab72e9fefc11d2db0b4ee1ceaccdbe
Hardyx07 <hardik.tailor@kalvium.community>
2025-04-08 15:46:16 +0530

    DataBase schema created

--- backend/models/Resource.ts
+++ backend/models/Resource.ts
@@ -5,8 +5,16 @@ const resourceSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     isFree: { type: Boolean, required: true },
-    price: {type: Number, required: function () {
-        return !this.isFree;
+    price: {type: Number,
+      validate: {
+        validator: function (value: number) {
+          // If not free, price must be provided
+          if (!this.isFree && (value === undefined || value === null)) {
+            return false;
+          }
+          return true;
+        },
+        message: 'Price is required if the resource is paid',
       },
     },
     file: { type: String }, // will store file path later
