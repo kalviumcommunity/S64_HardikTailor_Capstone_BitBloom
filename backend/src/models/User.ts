1c608bd7a50fc0365c28026332f20723e3b507c2
Hardyx07 <hardik.tailor@kalvium.community>
2025-04-11 16:29:21 +0530

    UserSchema Added

---
+++ backend/src/models/User.ts
@@ -0,0 +1,32 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  username: string;
+  email: string;
+  password: string; // Hashed password
+}
+
+const UserSchema: Schema<IUser> = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false, // hides password from query by default
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
